788f94a4515f32f846217e4ed7169b25a83cc06c
PauloGandraSousa <pag@isep.ipp.pt>
2013-03-10 19:35:05 +0000

    added loop in main menu

--- eapli.expensemanager/src/Presentation/MainMenu.java
+++ eapli.expensemanager/src/Presentation/MainMenu.java
@@ -11,23 +11,26 @@ import eapli.util.Console;
  * @author Paulo Gandra Sousa
  */
 public class MainMenu {
+
     public void mainLoop() {
-        System.out.println("===================");
-        System.out.println("  EXPENSE MANAGER  ");
-        System.out.println("===================\n");
-        
-        System.out.println("1. Register an expense");
-        System.out.println("0. Exit\n\n");
-        
-        int option = Console.readInteger("Please choose a option");
-        switch (option) {
-            case 0: 
-                System.out.println("bye bye ...");
-                return;
-            case 1: 
-                ExpenseRegisterUI ui = new ExpenseRegisterUI();
-                ui.mainLoop();
-                break;
-        }
+        do {
+            System.out.println("===================");
+            System.out.println("  EXPENSE MANAGER  ");
+            System.out.println("===================\n");
+
+            System.out.println("1. Register an expense");
+            System.out.println("0. Exit\n\n");
+
+            int option = Console.readInteger("Please choose a option");
+            switch (option) {
+                case 0:
+                    System.out.println("bye bye ...");
+                    return;
+                case 1:
+                    ExpenseRegisterUI ui = new ExpenseRegisterUI();
+                    ui.mainLoop();
+                    break;
+            }
+        } while (true);
     }
 }
