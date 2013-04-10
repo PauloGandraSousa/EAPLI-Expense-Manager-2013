2fe663f8646dd26155f226fd670245889c906842
losa <losa@losa-PC>
2013-04-10 12:41:00 +0100

    teste de git

--- eapli.util/src/eapli/util/Math.java
+++ eapli.util/src/eapli/util/Math.java
@@ -12,4 +12,11 @@ public class Math {
     public static boolean isOdd(int i) { return i % 2 != 0; }
     
     public static boolean isEven(int i) { return i % 2 == 0; }
+    
+    
+    public static int subtractone ( int i)
+    {
+        return i-1;
+        
+    }
 }
