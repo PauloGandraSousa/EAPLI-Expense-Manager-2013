d73eaa4159cdbc4215c0844a98b610070fa7e5f1
Nuno Bettencourt <nmb@isep.ipp.pt>
2013-04-18 20:44:19 +0100

    Corrigido o método doShow que não permitia registar uma despesa que estivesse na posição 0 do index.

--- eapli.expensemanager/src/eapli/expensemanager/presentation/RegisterExpenseUI.java
+++ eapli.expensemanager/src/eapli/expensemanager/presentation/RegisterExpenseUI.java
@@ -17,19 +17,20 @@ import java.util.List;
  * @author Paulo Gandra Sousa
  */
 class RegisterExpenseUI extends BaseUI {
+
     @Override
     public String headline() {
-        return "REGISTER AN EXPENSE";        
+        return "REGISTER AN EXPENSE";
     }
-    
+
     @Override
     public boolean doShow() {
         String what = Console.readLine("What:");
         Date date = Console.readDate("When (dd-MM-yyyy):");
         double value = Console.readDouble("How much:");
         BigDecimal amount = new BigDecimal(value);
-        
-        System.out.println("-- EXPENSE TYPES --");    
+
+        System.out.println("-- EXPENSE TYPES --");
         // TODO remove duplicated code block also present in ListExpenseTypesUI
         int position = 1;
         List<ExpenseType> listExpenseTypes = controller.getExpenseTypes();
@@ -38,15 +39,16 @@ class RegisterExpenseUI extends BaseUI {
             position++;
         }
         int option = Console.readOption(1, position, 0);
-              
-        controller.registerExpense(what, date, amount, listExpenseTypes.get(option));
-          
+
+        //NMB: corrigida a questão do index das despesas que não permitia obter 
+        // o primeiro tipo de despesa
+        controller.registerExpense(what, date, amount, listExpenseTypes.get(option - 1));
+
         System.out.println("\nExpense recorded!");
-        
+
         return true;
     }
-
-            RegisterExpenseController controller = new RegisterExpenseController();
+    RegisterExpenseController controller = new RegisterExpenseController();
 
     @Override
     protected BaseController controller() {
