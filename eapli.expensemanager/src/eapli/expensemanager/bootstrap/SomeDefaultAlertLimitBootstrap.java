1b7799696c1fa08f62a7d1813c82ca477686bd23
PauloGandraSousa <pag@isep.ipp.pt>
2013-05-08 16:53:13 +0000

    minor code review

--- eapli.expensemanager/src/eapli/expensemanager/bootstrap/SomeDefaultAlertLimitBootstrap.java
+++ eapli.expensemanager/src/eapli/expensemanager/bootstrap/SomeDefaultAlertLimitBootstrap.java
@@ -4,7 +4,6 @@
  */
 package eapli.expensemanager.bootstrap;
 
-
 import eapli.expensemanager.model.observer.AlertLimitExpenditure;
 import eapli.expensemanager.model.observer.AlertLimitType;
 import eapli.expensemanager.persistence.AlertLimitRepository;
@@ -16,31 +15,37 @@ import java.math.BigDecimal;
  * @author mcn
  */
 public class SomeDefaultAlertLimitBootstrap {
- private final BigDecimal LIMITWEEKYELLOW = new BigDecimal(100);
-      private final BigDecimal LIMITWEEKRED = new BigDecimal(500);
-      private final BigDecimal LIMITMONTHYELLOW = new BigDecimal(600);
-      private final BigDecimal LIMITMONTHRED = new BigDecimal(2000);
 
-      public SomeDefaultAlertLimitBootstrap() {
-            AlertLimitType[] types = AlertLimitType.values();
-            int size = types.length;
-            if (size > 0) {
-                  AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
-                  for (AlertLimitType alertLimitType : types) {
-                        switch (alertLimitType) {
-                              case LIMITWEEKEXPENDITURE:
-                                    if (AlertLimitExpenditure.findByAlertType(alertLimitType) == null) {
-                                          repo.save(new AlertLimitExpenditure(alertLimitType, LIMITWEEKYELLOW, LIMITWEEKRED));
-                                    }
-                                    break;
-                              case LIMITMONTHEXPENDITURE:
-                                    if (AlertLimitExpenditure.findByAlertType(alertLimitType) == null) {
-                                          repo.save(new AlertLimitExpenditure(alertLimitType, LIMITMONTHYELLOW, LIMITMONTHRED));
-                                    }
-                                    break;
+    private final BigDecimal LIMITWEEKYELLOW = new BigDecimal(100);
+    private final BigDecimal LIMITWEEKRED = new BigDecimal(500);
+    private final BigDecimal LIMITMONTHYELLOW = new BigDecimal(600);
+    private final BigDecimal LIMITMONTHRED = new BigDecimal(2000);
 
+    public SomeDefaultAlertLimitBootstrap() {
+        AlertLimitType[] types = AlertLimitType.values();
+        int size = types.length;
+        if (size > 0) {
+            AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
+            for (AlertLimitType alertLimitType : types) {
+                switch (alertLimitType) {
+                    case LIMITWEEKEXPENDITURE:
+                        if (AlertLimitExpenditure.findByAlertType(alertLimitType) == null) {
+                            // TODO if alerts are Active Records, from the outside 
+                            // we should not call repository methods but the object's
+                            // save method
+                            repo.save(new AlertLimitExpenditure(alertLimitType, LIMITWEEKYELLOW, LIMITWEEKRED));
                         }
-                  }
+                        break;
+                    case LIMITMONTHEXPENDITURE:
+                        if (AlertLimitExpenditure.findByAlertType(alertLimitType) == null) {
+                            // TODO if alerts are Active Records, from the outside 
+                            // we should not call repository methods but the object's
+                            // save method
+                            repo.save(new AlertLimitExpenditure(alertLimitType, LIMITMONTHYELLOW, LIMITMONTHRED));
+                        }
+                        break;
+                }
             }
-      }
+        }
+    }
 }
--- eapli.expensemanager/src/eapli/expensemanager/model/observer/AlertLimitExpenditure.java
+++ eapli.expensemanager/src/eapli/expensemanager/model/observer/AlertLimitExpenditure.java
@@ -12,79 +12,76 @@ import java.util.List;
 import java.util.Locale;
 import javax.persistence.Entity;
 
-
 /**
  *
  * @author mcn
  */
 @Entity
-public class AlertLimitExpenditure extends AlertLimit  {
+public class AlertLimitExpenditure extends AlertLimit {
 
-      protected BigDecimal limitYellow;
-      protected BigDecimal limitRed;
+    protected BigDecimal limitYellow;
+    protected BigDecimal limitRed;
 
-      public AlertLimitExpenditure() {
-      }
+    public AlertLimitExpenditure() {
+    }
 
-      public BigDecimal getLimitYellow() {
-            return limitYellow;
-      }
+    public BigDecimal getLimitYellow() {
+        return limitYellow;
+    }
 
-      public BigDecimal getLimitRed() {
-            return limitRed;
-      }
+    public BigDecimal getLimitRed() {
+        return limitRed;
+    }
 
-      public AlertLimitExpenditure(AlertLimitType alertType, BigDecimal limitYellow, BigDecimal limitRed) {
-            super(alertType);
-            this.limitYellow = limitYellow;
-            this.limitRed = limitRed;
-      }
+    public AlertLimitExpenditure(AlertLimitType alertType, BigDecimal limitYellow, BigDecimal limitRed) {
+        super(alertType);
+        this.limitYellow = limitYellow;
+        this.limitRed = limitRed;
+    }
 
-      public void setLimitYellow(BigDecimal limitYellow) {
-            this.limitYellow = limitYellow;
-      }
+    public void setLimitYellow(BigDecimal limitYellow) {
+        this.limitYellow = limitYellow;
+    }
 
-      public void setLimitRed(BigDecimal limitRed) {
-            this.limitRed = limitRed;
-      }
+    public void setLimitRed(BigDecimal limitRed) {
+        this.limitRed = limitRed;
+    }
 
-      @Override
-      public String toString() {
+    @Override
+    public String toString() {
 
-            NumberFormat n = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-            double doubleLimitYellow = this.limitYellow.doubleValue();
-            double doubleLimitRed = this.limitRed.doubleValue();
-            String str = super.toString()
-                    + "\nYellow Limit:" + n.format(doubleLimitYellow) + "\nRed Limit:" + n.format(doubleLimitRed);
-            return str;
-      }
+        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        double doubleLimitYellow = this.limitYellow.doubleValue();
+        double doubleLimitRed = this.limitRed.doubleValue();
+        String str = super.toString()
+                + "\nYellow Limit:" + n.format(doubleLimitYellow) + "\nRed Limit:" + n.format(doubleLimitRed);
+        return str;
+    }
 
-      // FIX this method is to DB oriented. it migth make sense to have it as a business
-      // method mut should have a more busines oriented name (e.g., updateLimits)
-      // and NOT call any repository methods
-      public void updateLimits(double limitYellow, double limitRed) {
+    public void updateLimits(double limitYellow, double limitRed) {
 
-            BigDecimal yellow = new BigDecimal(limitYellow);
-            BigDecimal red = new BigDecimal(limitRed);
-            this.limitYellow=yellow;
-            this.limitRed=red;
-            AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
-            repo.update(this);
-      }
-      
+        BigDecimal yellow = new BigDecimal(limitYellow);
+        BigDecimal red = new BigDecimal(limitRed);
+        this.limitYellow = yellow;
+        this.limitRed = red;
+        // FIX this method should NOT call any repository methods but rely on the 
+        // client code to call the object's save() method
+        AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
+        repo.update(this);
+    }
+
 //            @Override
 //      public void update() {
 //            AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
 //            repo.update(this);
 //      }
-
-      public static AlertLimitExpenditure findByAlertType(AlertLimitType aLertType) {
-            AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
-            List<AlertLimitExpenditure> list = repo.findByAlertType(aLertType);
-            if (list.isEmpty()) {
-                  return null;
-            }
-            return list.get(0);
-      }
-
+    
+    public static AlertLimitExpenditure findByAlertType(AlertLimitType aLertType) {
+        AlertLimitRepository repo = PersistenceFactory.buildPersistenceFactory().alertLimitRepository();
+        List<AlertLimitExpenditure> list = repo.findByAlertType(aLertType);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
 }
