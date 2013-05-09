cc7c33e19709f066d56ee8507f5e832a7cc391ca
PauloGandraSousa <pag@isep.ipp.pt>
2013-05-09 11:54:39 +0000

    code review

--- eapli.expensemanager/src/eapli/expensemanager/presentation/RegisterSavingWithdrawUI.java
+++ eapli.expensemanager/src/eapli/expensemanager/presentation/RegisterSavingWithdrawUI.java
@@ -14,36 +14,35 @@ import java.util.List;
  *
  * @author AJS
  */
-public class RegisterSavingWithdrawUI extends RegisterMovementBaseUI
-{
+public class RegisterSavingWithdrawUI extends RegisterMovementBaseUI {
+
     SelectWidget widget;
 
     @Override
-    public String headline()
-    {
+    public String headline() {
         return "REGISTER AN SAVING WITHDRAW";
     }
 
     @Override
-    public boolean doShow()
-    {
+    public boolean doShow() {
+        // FIX this code is duplicated with RegisterSavingDepositUI
         SavingGoal savingGoal = readSavingGoal();
         readGeneralData();
+
         controller.registerSavingWithdraw(savingGoal, date, amount, what);
         System.out.println("\nSaving Withdraw recorded!");
         return true;
     }
-
+    
     RegisterSavingWithdrawController controller = new RegisterSavingWithdrawController();
 
     @Override
-    protected BaseController controller()
-    {
+    protected BaseController controller() {
         return controller;
     }
 
-    private SavingGoal readSavingGoal()
-    {
+    // FIX this code is duplicated with RegisterSavingDepositUI
+    private SavingGoal readSavingGoal() {
         System.out.println("-- SAVING GOAL --");
         List<SavingGoal> listSavingGoal = controller.getSavingGoals();
 
