f80856c008a3b4c25f0063817e25fa26447327de
PauloGandraSousa <pag@isep.ipp.pt>
2013-05-09 22:06:52 +0000

    catch InsufficientBalanceException

--- eapli.expensemanager/src/eapli/expensemanager/presentation/RegisterExpenseUI.java
+++ eapli.expensemanager/src/eapli/expensemanager/presentation/RegisterExpenseUI.java
@@ -11,11 +11,14 @@ import eapli.expensemanager.model.ExpenseType;
 import eapli.expensemanager.model.Payment;
 import eapli.expensemanager.model.PaymentMean;
 import eapli.expensemanager.model.AlertEvent;
+import eapli.expensemanager.model.InsufficientBalanceException;
 import eapli.framework.presentation.SelectWidget;
 import eapli.util.Console;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -35,20 +38,23 @@ class RegisterExpenseUI extends RegisterMovementBaseUI implements Observer {
         readGeneralData();
 
         ExpenseType expenseType = readExpenseType();
-        
+
         // TODO should the UI create a Payment object?
-        Payment method = readPaymentMean();        
-        
-          // mcn: delegar no controller o pedido de registo de Observador de WatchDogLimits 
-            controller.addObserverWatchDogLimits(this);
+        Payment method = readPaymentMean();
 
-        controller.registerExpense(what, date, amount, expenseType, method);
+        // mcn: delegar no controller o pedido de registo de Observador de WatchDogLimits 
+        controller.addObserverWatchDogLimits(this);
+        try {
+            controller.registerExpense(what, date, amount, expenseType, method);
+            System.out.println("\nExpense recorded!");
+        } catch (InsufficientBalanceException ex) {
+            Logger.getLogger(RegisterExpenseUI.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Unable to register expense due to insufficient balance");
+        }
 
-        System.out.println("\nExpense recorded!");
 
         return true;
     }
-    
     RegisterExpenseController controller = new RegisterExpenseController();
 
     @Override
@@ -63,8 +69,8 @@ class RegisterExpenseUI extends RegisterMovementBaseUI implements Observer {
         expenseTypesSelectWidget = new SelectWidget<ExpenseType>(listExpenseTypes, new ExpenseTypeListVisitor());
         expenseTypesSelectWidget.show();
         int option = expenseTypesSelectWidget.selectedOption();
-        
-        ExpenseType expenseType = listExpenseTypes.get(option-1);
+
+        ExpenseType expenseType = listExpenseTypes.get(option - 1);
         return expenseType;
     }
 
@@ -78,31 +84,29 @@ class RegisterExpenseUI extends RegisterMovementBaseUI implements Observer {
             position++;
         }
         int option = Console.readOption(1, paymentMeans.size(), 0);
-        PaymentMean method = paymentMeans.get(option-1);
-        
+        PaymentMean method = paymentMeans.get(option - 1);
+
         // TODO should the UI ask for a Payment object or call different 
         // registerXXX paymentMeans on the controller, i.e., registerExpensePaidwithCheque
         // or registerExpensePaidWithCash, etc...
         Payment payment;
         if (method.getClass() == Cheque.class) {
             String chequeNumber = Console.readLine("Cheque number:");
-            payment = controller.createChequePayment((Cheque)method, chequeNumber);
-        }
-        else {
+            payment = controller.createChequePayment((Cheque) method, chequeNumber);
+        } else {
             payment = controller.createPayment(method);
         }
-        
+
         return payment;
     }
-    
-    // Observer pattern
-          @Override
-      public void update(Observable o, Object arg) {
-          AlertEvent al=(AlertEvent)arg;
-            System.out.println("************************************ALERT*****************************");
-            System.out.println(al);
-            System.out.println("**********************************************************************");
 
-      }
-    
+    // Observer pattern
+    @Override
+    public void update(Observable o, Object arg) {
+        AlertEvent al = (AlertEvent) arg;
+        System.out.println("************************************ALERT*****************************");
+        System.out.println(al);
+        System.out.println("**********************************************************************");
+
+    }
 }
