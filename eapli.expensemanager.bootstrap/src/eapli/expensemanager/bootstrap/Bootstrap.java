1f5fab0fe9a650afa20f74f8f4fd4824fa76edd3
PauloGandraSousa <pag@isep.ipp.pt>
2013-06-03 13:58:27 +0000

    created bootstrap project

---
+++ eapli.expensemanager.bootstrap/src/eapli/expensemanager/bootstrap/Bootstrap.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.expensemanager.bootstrap;
+
+/**
+ *
+ * @author Paulo Gandra Sousa
+ */
+public interface Bootstrap {
+
+    /**
+     * ensure the desired bootstrap data exists or if not create it
+     */
+    void bootstrap();
+}
---
+++ eapli.expensemanager.bootstrap/src/eapli/expensemanager/bootstrap/SomeIncomesBootstrap.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.expensemanager.bootstrap;
+
+import eapli.expensemanager.model.CheckingAccount;
+import eapli.expensemanager.model.Income;
+import eapli.expensemanager.model.IncomeType;
+import eapli.expensemanager.persistence.CheckingAccountRepository;
+import eapli.expensemanager.persistence.IncomeTypeRepository;
+import eapli.expensemanager.persistence.PersistenceFactory;
+import eapli.util.DateTime;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Paulo Gandra Sousa
+ */
+public class SomeIncomesBootstrap implements Bootstrap {
+
+    @Override
+    public void bootstrap() {
+        CheckingAccountRepository repoAccount = PersistenceFactory.buildPersistenceFactory().checkingAccountRepository();
+        CheckingAccount theAccount = repoAccount.theAccount();
+
+        IncomeTypeRepository repoIncomeType = PersistenceFactory.buildPersistenceFactory().incomeTypeRepository();
+        IncomeType salary = repoIncomeType.findForName(ReferenceDataBootstrap.SALARY_INCOME_TYPE);
+
+        Calendar baseDateOfIncome = DateTime.today();
+        Date dateOfIncome = baseDateOfIncome.getTime();
+
+        Income inc = new Income("ordenado deste mês", dateOfIncome, new BigDecimal(1000), salary);
+        theAccount.registerIncome(inc);
+
+        repoAccount.save(theAccount);
+    }
+}
