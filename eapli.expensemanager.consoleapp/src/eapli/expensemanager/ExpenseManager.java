e10f69bb29c69e946a8344184de0a5479dde7d85
PauloGandraSousa <pag@isep.ipp.pt>
2013-06-03 16:30:07 +0000

    updated Eclipse projects for new project structure

--- eapli.expensemanager.consoleapp/src/eapli/expensemanager/ExpenseManager.java
+++ eapli.expensemanager.consoleapp/src/eapli/expensemanager/ExpenseManager.java
@@ -9,40 +9,36 @@ import eapli.expensemanager.bootstrap.SomeDefaultAlertLimitBootstrap;
 import eapli.expensemanager.bootstrap.SomeExpensesBootstrap;
 import eapli.expensemanager.bootstrap.SomeIncomesBootstrap;
 import eapli.expensemanager.presentation.MainMenu;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
- *
+ * 
  * @author Paulo Gandra Sousa
  */
 public final class ExpenseManager {
 
-    private ExpenseManager() {
-    }
+	private ExpenseManager() {
+	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // call the bootstraper
-        // the next line should be removed for "production" ready deployment
-        doBootstrap();
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		// call the bootstraper
+		// the next line should be removed for "production" ready deployment
+		doBootstrap();
 
-        final MainMenu menu = new MainMenu();
-        menu.mainLoop();
-    }
+		final MainMenu menu = new MainMenu();
+		menu.mainLoop();
+	}
 
-    private static void doBootstrap() {
-        new ReferenceDataBootstrap().bootstrap();
+	private static void doBootstrap() {
+		new ReferenceDataBootstrap().bootstrap();
 
-        new SomeIncomesBootstrap().bootstrap();
+		new SomeIncomesBootstrap().bootstrap();
 
-        new SomeExpensesBootstrap().bootstrap();
+		new SomeExpensesBootstrap().bootstrap();
 
-        new SomeDefaultAlertLimitBootstrap().bootstrap();
-    }
+		new SomeDefaultAlertLimitBootstrap().bootstrap();
+	}
 }
